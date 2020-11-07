b1ecad1f25b421c99007817dae1a5410d3d16b0d
CharlesMickey <charlesmickey.ru@yandex.ru>
2020-11-07 12:29:26 +0300

    feat: создать popup

--- script/index.js
+++ script/index.js
@@ -1,10 +1,20 @@
 let profile = document.querySelector(".profile");
 let profileProfileInfo = profile.querySelector(".profile__profile-info");
 let buttonOpenForm = profileProfileInfo.querySelector(".profile__edit-button");
-console.log(buttonOpenForm);
+let popup = document.querySelector(".popup");
+let popupContainer = popup.querySelector(".popup__container");
+let buttonCloseForm = popupContainer.querySelector(".popup__button-close");
+let paragraphL = document.querySelector(".popup__your-name");
+let paragraph = document.querySelector(".profile__name");
+let profileInterests = document.querySelector(".profile__interests");
+let interests = document.querySelector(".popup__interests");
+console.log(paragraph.textContent);
+paragraphL.value = paragraph.textContent;
+interests.value = profileInterests.textContent;
 
 function showClic() {
-alert("Миром правят собаки")
+popup.classList.toggle('popup_opened');
 }
 
 buttonOpenForm.addEventListener('click', showClic);
+buttonCloseForm.addEventListener('click', showClic);
